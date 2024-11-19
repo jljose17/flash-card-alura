5efba591d85a4d5c16cc0207cc0e8aa0db3344a6
jljose17 <00001091958191sp@al.educacao.sp.gov.br>
2024-11-19 17:19:27 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,31 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Espanha?',
+    'A capital da Espanha é Madrid'
+)
+
+
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se fala carro em Inglês?',
+    'Carro em ingles é Car (Car)'
+)
